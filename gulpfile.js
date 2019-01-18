08c5395e0d0d397437b138e79af5a9bc7cdcd8bd
Andre Jesus <andrejesus@hotmail.com>
2019-01-18 11:07:40 +0000

    WIP main structure, demo page, docs page, buttons

--- gulpfile.js
+++ gulpfile.js
@@ -17,46 +17,32 @@ const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 
 
-gulp.task('default', ['sass', 'babel'], () => {
+gulp.task('default', ['sass'], () => {
     browserSync.init({
         server: {
-            baseDir: "./demo"
+            baseDir: "./",
+            index: "demo.html"
         }
     });
     gulp.watch("./demo/*.html").on('change', browserSync.reload);
+    gulp.watch('style.scss', ['sass']);
     gulp.watch('./elements/**/*.scss', ['sass']);
-    gulp.watch('./elements/**/*.js', ['babel']);
 });
 
 gulp.task('sass', ['sass-min'], () => {
     const plugins = [autoprefixer({browsers: ['last 2 versions']})];
-    return gulp.src('./elements/*.scss')
+    return gulp.src('style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(plugins))
-        .pipe(gulp.dest('./demo/css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('sass-min', () => {
     const plugins = [autoprefixer({browsers: ['last 2 versions']}), cssnano()];
-    return gulp.src('./elements/*.scss')
+    return gulp.src('style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(plugins))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
-});
-
-
-gulp.task('babel', function() {
-    browserify({
-        entries: './src/index.js',
-        debug: true
-    })
-    .transform(babelify, { presets: ['env'] })
-    .on('error',gutil.log)
-    .bundle()
-    .on('error',gutil.log)
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream());
 });
